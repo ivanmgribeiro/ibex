945d80e5d1563ed509aac1dc45bc73d02d56e42f
Ivan Ribeiro <ivanmgribeiro@google.com>
2022-09-15 15:36:36 -0700

    [rtl] Allow ifetches that overflow address calcs
    The sail model allows instruction fetches that overflow the address
    space calculation (ie requesting the instruction at 'hFFFF_FFFE).
    This commit aligns ibex to that behaviour.
    
    Change-Id: Ic54bf3ba59c9cd5f10653bcbd08b4067d9d7c689

--- rtl/ibex_cheri_memchecker.sv
+++ rtl/ibex_cheri_memchecker.sv
@@ -44,7 +44,7 @@ module ibex_cheri_memchecker #(
   // get data size from type to use in bounds checking, and then zero-extend
   // it to the correct size (33 bits since capability "top" is 33 bits)
   logic [3:0]  data_size;
-  logic [32:0] data_size_ext;
+  logic [31:0] data_size_ext;
   if (DataMem) begin
     assign data_size = data_type_i == 2'b00 ? 4'h4 : // Word
                        data_type_i == 2'b01 ? 4'h2 : // Halfword
@@ -53,7 +53,7 @@ module ibex_cheri_memchecker #(
   end else begin
     assign data_size = 4'h2; // instruction accesses are 2 bytes
   end
-  assign data_size_ext = {29'h0, data_size};
+  assign data_size_ext = {28'h0, data_size};
 
   // calculate actual data start address using byte enable
   // upper bits are identical since Ibex only produces accesses aligned to 4 bytes
@@ -81,7 +81,10 @@ module ibex_cheri_memchecker #(
   assign cheri_mem_exc_d[  PERMIT_STORE_VIOLATION] =  data_we_i & ~auth_cap_getPerms_o[3];
   assign cheri_mem_exc_d[PERMIT_EXECUTE_VIOLATION] = ~DataMem & ~auth_cap_getPerms_o[PermitExecuteIndex];
   assign cheri_mem_exc_d[        LENGTH_VIOLATION] = (data_addr_actual < auth_cap_getBase_o)
-                                                     | ({1'b0, data_addr_actual} + data_size_ext > auth_cap_getTop_o);
+                                                     // if this is an instruction checker, allow accesses that
+                                                     // overflow the address calculation
+                                                     | DataMem ? ({1'b0, data_addr_actual} + {1'b0, data_size_ext} > auth_cap_getTop_o)
+                                                               : ({1'b0, data_addr_actual + data_size_ext} > auth_cap_getTop_o);
   // don't bother checking if it is below base (if it is, then there will be
   // a length violation in the lower word anyway
   assign instr_upper_exc_d                         = DataMem ? 0 : {1'b0, data_addr_actual_upper} >= auth_cap_getTop_o;
